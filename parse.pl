e0f61e03ba360de68e55494f06b85d0623521741
Conor Keegan <con@conor.net>
2012-02-26 14:08:16 +0000

    utf8 issues

--- parse.pl
+++ parse.pl
@@ -2,8 +2,10 @@
 
 use strict;
 use warnings;
+
 use LWP::UserAgent;
 use XML::RSS::Parser;
+
 
 my $self = {
         url => 'http://some.example/rss',
@@ -20,18 +22,20 @@ $ua->agent('Punctual Podcast Parser');
 $ua->from('info@conor.net');
 $ua->timeout('60');
 
-my $r = $ua->get($self->url);
-my $feed="";
+my $r = $ua->get($self->{url});
+#my $feed="";
 if($r->is_error){
 	#open (LOG, ">>".$self->{logFile}) || die "oh no! can't open ".$self->{logFile}."\n";
 	#select(LOG); $| = 1;
         #print LOG "$geturl => " . $r->status_line . "\n";
         #close (LOG);
         #return $ERROR_MSG;
-	}
-else {
-	$feed = $r->content;
+print "error ". $r->status_line . "\n";
 }
+
+my $p = XML::RSS::Parser->new;
+my $feed = $p->parse_string($r->content);
+#print "$feed";
 
 my $feed_title = $feed->query('/channel/title');
  print $feed_title->text_content;
@@ -39,7 +43,7 @@ my $feed_title = $feed->query('/channel/title');
  print " ($count)\n";
  foreach my $i ( $feed->query('//item') ) { 
      my $node = $i->query('title');
-     print '  '.$node->text_content;
+     print ' '.$node->text_content;
      print "\n"; 
  }
 
